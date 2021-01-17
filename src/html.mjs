f31d77c0713546a6a275a15442598df42db26d5f
nanlei.roy <nanlei.roy@bytedance.com>
2021-01-17 23:26:34 +0800

    html change

--- src/html.mjs
+++ src/html.mjs
@@ -1,7 +1,7 @@
 import { fileURLToPath } from 'url';
 import path from "path";
 import fs from "fs";
-
+import childProcess from 'child_process';
 const SEP_TAG = "<!-- SEP-1511151742953336 -->";
 const filePath = path.resolve(fileURLToPath(import.meta.url), "../../index.html");
 
@@ -10,8 +10,12 @@ function readHTML() {
   return html;
 }
 
+function commitChanges() {
+  childProcess.execSync('git add .');
+  childProcess.execSync('git commit -m \'html change\'');
+}
+
 /**
- * 
  * @param str {string}
  * @param html {string}
  */
@@ -20,5 +24,6 @@ export function appendToHTML(str) {
   const [head, tail] = html.split(SEP_TAG);
   const modifiedHtml = [head, str + tail].join(SEP_TAG + "\n");
   fs.writeFileSync(filePath, modifiedHtml);
+  commitChanges();
   return modifiedHtml;
 }
